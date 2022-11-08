ef4c824ffae68ceda58c2930df75217b93ff8d2a
Safwan <safwanridwan321@gmail.com>
2022-11-08 15:07:41 +0600

    added service id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -13,7 +13,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
+async function run() {
     try {
         await client.connect();
         console.log('mongodb connected successfully')
@@ -29,8 +29,29 @@ app.get('/', (req, res) => {
     res.send('eKitchen server is running fine');
 })
 
+app.get('/services', async (req, res) => {
+    try {
+        const limit = parseInt(req.query.limit);
+        const query = {};
+        const cursor = ServiceCollection.find(query);
+        const data = await cursor.limit(limit).toArray();
+        console.log(data)
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
+})
 
-
+app.get('/services/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const data = await ServiceCollection.findOne(query)
+        res.send(data);
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 
 
